c07f383f91ff2dba40311e58c6bd5b764a922758
Rutvi Gaudani <rutvigaudani11@gmail.com>
2022-09-14 12:17:04 +0530

    done

--- src/app.js
+++ src/app.js
@@ -16,21 +16,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 //const upload = require('express-fileupload');
 //app.use('/upload', express.static('upload'));
-
 //app.use(upload())
-//         const fileStorageEngine = multer.diskStorage({
-//             destination:(req,file,cb)=>{
-//               cb(null,'./images')
-//             },
-//             filename:(req,file,cb)=>{
-//               cb(null,Date.now()+'--'+file.originalname)
-//             }
-//           });const upload =multer({storage:fileStorageEngine});
-//           app.post("/single",upload.single("image"),(req,res)=>{
-//             console.log(req.file);
-//             res.send("file uploaded");
-//           });
-
 app.listen(3000, () => {
 
     console.log("server running on port:3000");
@@ -39,11 +25,3 @@ conn.on('open', () => {
     console.log("connected....");
 })
 
-// const xlsx = require('xlsx');
-// const wb = xlsx.readFile("./data/1663128661324-customer.xlsx");
-// console.log(wb.SheetNames);
-// const ws = wb.Sheets['Sheet1'];
-// //console.log(ws);
-
-// const data =xlsx.utils.sheet_to_json(ws);
-// console.log(data);
